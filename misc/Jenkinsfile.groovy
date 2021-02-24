5bc87aa42115a664bcdee726d8b9f37b99b14185
adantop <40446785+adantop@users.noreply.github.com>
2021-02-23 19:58:16 -0600

    Update Jenkinsfile.groovy

--- misc/Jenkinsfile.groovy
+++ misc/Jenkinsfile.groovy
@@ -6,13 +6,11 @@ node {
       commit_id = readFile('.git/commit-id').trim()
    }
    stage('test') {
-      docker.withRegistry('https://registry-1.docker.io/v2', 'dockerhub') {
-         def myTestContainer = docker.image('node:4.6')        
-         myTestContainer.pull()
-         myTestContainer.inside {
-            sh 'npm install --only=dev'
-            sh 'npm test'
-         }
+      def myTestContainer = docker.image('node:4.6')        
+      myTestContainer.pull()
+      myTestContainer.inside {
+         sh 'npm install --only=dev'
+         sh 'npm test'
       }
    }
    /* stage('test with a DB') {
