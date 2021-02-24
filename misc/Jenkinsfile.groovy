dcac0a5cb51f71bb1bd21bb0ca85bb10cd3fdaa8
adantop <40446785+adantop@users.noreply.github.com>
2021-02-23 22:21:19 -0600

    Update Jenkinsfile.groovy

--- misc/Jenkinsfile.groovy
+++ misc/Jenkinsfile.groovy
@@ -1,28 +1,22 @@
 node {
    def commit_id
    
-   stage('setup') {
-      checkout scm
-      sh 'git rev-parse --short HEAD > .git/commit-id'
-      commit_id = readFile('.git/commit-id').trim()
-   }
-   
-   stage('unit-test') {
-      docker.withServer('tcp://192.168.0.146:2376', 'localDocker') {
-         docker.withRegistry('', 'dockerhub') {
-            
+   docker.withServer('tcp://192.168.0.146:2376', 'localDocker') {
+      docker.withRegistry('', 'dockerhub') {
+         stage('setup') {
+            checkout scm
+            sh 'git rev-parse --short HEAD > .git/commit-id'
+            commit_id = readFile('.git/commit-id').trim()
+         }
+
+         stage('unit-test') {
             docker.image('node:4.6').inside {
                sh 'npm install --only=dev'
                sh 'npm test'
             }
          }
-      }
-   }
-   
-   stage('integration-test') {
-      docker.withServer('tcp://192.168.0.146:2376', 'localDocker') {
-         docker.withRegistry('', 'dockerhub') {
-            
+
+         stage('integration-test') {
             docker.image('mysql').withRun("-e MYSQL_ALLOW_EMPTY_PASSWORD=yes") { mysql ->
                // using linking, mysql will be available at host: mysql, port: 3306
                docker.image('node:4.6').inside("--link ${mysql.id}:mysql") {
